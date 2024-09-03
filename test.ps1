259ee93e4c2ebde1ac3315c79d1b40ddd27d8c9a
Tom <92548671+BrainButcher101@users.noreply.github.com>
2024-09-03 22:53:52 +0200

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,83 @@
+# Import the VMware PowerCLI module
+Import-Module VMware.VimAutomation.Core
+
+# Connect to the vCenter Server
+$vcenterServer = "vcenter.yourdomain.com"
+$vcUsername = "administrator@vsphere.local"
+$vcPassword = "YourPassword"
+Connect-VIServer -Server $vcenterServer -User $vcUsername -Password $vcPassword
+
+# Parameters
+$datacenterName = "YourDatacenter"
+$clusterName = "YourCluster"
+$datastoreName = "YourDatastore"
+$vmTemplate = "YourVMTemplate"
+$networkNameTestServer = "VLAN-TestServer-PortGroup"  # VLAN where the test server will be
+$vmNameTestServer = "TestServer"
+$vlans = @("VLAN1-PortGroup", "VLAN2-PortGroup", "VLAN3-PortGroup", "VLAN4-PortGroup", "VLAN5-PortGroup", "VLAN6-PortGroup", "VLAN7-PortGroup", "VLAN8-PortGroup", "VLAN9-PortGroup", "VLAN10-PortGroup", "VLAN11-PortGroup", "VLAN12-PortGroup", "VLAN13-PortGroup", "VLAN14-PortGroup", "VLAN15-PortGroup", "VLAN16-PortGroup", "VLAN17-PortGroup", "VLAN18-PortGroup", "VLAN19-PortGroup", "VLAN20-PortGroup")
+$vmCpu = 2
+$vmMemoryGB = 4
+$vmDiskGB = 40
+
+# Function to create VM
+function Create-VM {
+    param (
+        [string]$vmName,
+        [string]$networkName
+    )
+
+    New-VM -Name $vmName `
+           -Template $vmTemplate `
+           -Datastore $datastoreName `
+           -NetworkName $networkName `
+           -Cluster $clusterName `
+           -DiskGB $vmDiskGB `
+           -MemoryGB $vmMemoryGB `
+           -NumCpu $vmCpu `
+           -ResourcePool (Get-Cluster $clusterName | Get-ResourcePool | Where-Object { $_.Name -eq 'Resources' }) `
+           -Datacenter $datacenterName -RunAsync
+}
+
+# Create the test server in the specified VLAN
+Write-Host "Creating Test Server in VLAN Test Server..."
+$testServer = Create-VM -vmName $vmNameTestServer -networkName $networkNameTestServer
+
+# Wait for the test server to be created
+Start-Sleep -Seconds 120
+
+# Get the IP Address of the Test Server
+$testServerIp = (Get-VM $vmNameTestServer | Get-VMGuest).IPAddress[0]
+
+# Create VMs in each of the other VLANs and ping from the Test Server
+foreach ($vlan in $vlans) {
+    $vmName = "TestVM-$vlan"
+    Write-Host "Creating VM in $vlan..."
+    $vm = Create-VM -vmName $vmName -networkName $vlan
+
+    # Wait for the VM to be created
+    Start-Sleep -Seconds 120
+
+    # Get IP Address of the VM
+    $vmIp = (Get-VM $vmName | Get-VMGuest).IPAddress[0]
+
+    # Perform Ping Test
+    function Test-Ping {
+        param (
+            [string]$sourceVmIp,
+            [string]$targetVmIp
+        )
+
+        Write-Host "Pinging from $sourceVmIp to $targetVmIp..."
+        Test-Connection -Source $sourceVmIp -ComputerName $targetVmIp -Count 4 -Quiet
+    }
+
+    # Ping from Test Server to the VM
+    if (Test-Ping -sourceVmIp $testServerIp -targetVmIp $vmIp) {
+        Write-Host "Ping from Test Server ($testServerIp) to VM ($vmIp) in $vlan succeeded."
+    } else {
+        Write-Host "Ping from Test Server ($testServerIp) to VM ($vmIp) in $vlan failed."
+    }
+}
+
+# Disconnect from vCenter
+Disconnect-VIServer -Server $vcenterServer -Confirm:$false
